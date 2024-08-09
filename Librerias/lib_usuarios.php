7079797772f9075b3aa5fcb5e8c19ccfb8484499
camilo <marrugobarrioscamilo2005gmail.com>
2024-08-09 16:48:27 -0500

    iniciando con el login

--- Librerias/lib_usuarios.php
+++ Librerias/lib_usuarios.php
@@ -45,68 +45,66 @@ SQL
         }
     } else {
         echo "campos vacios, porfavor llene los campos";
-        
+
     }
 
 }
 
-function Eliminar (){
-        include_once "../conexion.php";
-        $conexion = Conexion();
+function Eliminar()
+{
+    include_once "../conexion.php";
+    $conexion = Conexion();
 
-        $id = $_GET["id"];
+    $id = $_GET["id"];
 
-        $id = pg_escape_string($id); // Asegúrate de escapar el ID para evitar inyecciones SQL
+    $id = pg_escape_string($id); // Asegúrate de escapar el ID para evitar inyecciones SQL
 
-        $consulta = <<<SQL
+    $consulta = <<<SQL
         DELETE FROM usuarios WHERE id = $id
 SQL;
-        $resultado = pg_query($conexion, $consulta);
-        if ($resultado) {
-            header("Location: ../vistas/usuarios/usuarios.php");
-        }
-        else{
-            echo "error ";
-        }
+    $resultado = pg_query($conexion, $consulta);
+    if ($resultado) {
+        header("Location: ../vistas/usuarios/usuarios.php");
+    } else {
+        echo "error ";
+    }
 
 }
 
 
+
+
 function Login(){
-    $html=<<<HTML
 
-    
-<!DOCTYPE html>
-<html lang="en">
+    include_once "../conexion.php";
 
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>inicia sesion</title>
-</head>
+    $conexion = Conexion();
 
-<body>
-    <div class="mx-auto contenedor">
-        <div class="formulario_registro">
-            <form class="mx-auto col-4 p-3 " action="../../controlador/ControllerValidacion.php?accion=login" method="post">
-                <h2 class="text-center text-secondary"> bienvenido </h2>
-                <p class="text-center text-secondary"> inicia sesion </p>
-                <input class="form-control" placeholder="correo" required type="text" name="correo"><br><br>
-                <input class="form-control" placeholder="contraseña" required type="password" name="contraseña"><br><b>
-                    <input class="btn btn-primary" name="inicio" class="btn" type="submit" value="entrar"><br><br>
-                    <a class="mr-auto navbar-brand" href="../usuarios/formulario_registro.php">registro</a>
-            </form>
-        </div>
-    </div>
-    <a href="../pagina-principal/index.php">inicio</a>
-</body>
+    /*$datos = [
+        "correo" => $_POST['correo'],
+        "contraseña" => $_POST['contraseña'],
+    ];*/
 
-</html>
+    $correo = $_POST["correo"];
+    $contraseña = $_POST["contraseña"];
 
-HTML;
+    $correo = pg_escape_string($correo);
+    $contraseña = pg_escape_string($contraseña);
 
-echo $html;
+    $consulta = <<<SQL
+        SELECT correo,contraseña FROM usuarios WHERE correo = $correo AND contraseña = $contraseña
+SQL;
+echo $consulta;
+
+    $resultado = pg_query($conexion, $consulta);
+    $filas = pg_fetch_array($resultado);
+
+    if ($filas) {
+        echo "inicisate sesion";
+    }
+    else{
+        echo "error";
+    }
 }
 
 if ($accion == "registrar") {
@@ -114,6 +112,9 @@ if ($accion == "registrar") {
 }
 if ($accion == "eliminar") {
     Eliminar();
+}
+if ($accion == "login") {
+    Login();
 }
 
 ?>
--- vistas/pagina-principal/login.php
+++ vistas/pagina-principal/login.php
@@ -1,4 +1,4 @@
 <?PHP
-include_once "../../Librerias/lib_HTM-U.php";
-Login();
+include_once "../../Librerias/lib_HTML-U.php";
+Login_html();
 ?>
